77d0689e83adda6eba26fb119e971650cefd1617
Jarek Lipski <loomchild@loomchild.net>
2015-05-29 13:21:19 +0200

    add config handling, read version from git

--- icoin/__init__.py
+++ icoin/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from .core import bootstrap
+
+bootstrap.init()
+
+from .api import api
+
+app.register_blueprint(api)
+
 from .gui import gui
 
 app.register_blueprint(gui)
 
-app.config['SECRET_KEY'] = 'B0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
---
+++ icoin/core/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'B0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+TEST = "test"
+
+
+
